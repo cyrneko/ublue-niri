aa3d6229898ddea7f30579f2e7d396b8c189b219
cyrneko <cyrus42@disroot.org>
2025-05-07 19:19:18 +0200

    please

--- files/scripts/build-packages.fish
+++ files/scripts/build-packages.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set --global packages cargo gtk3-devel gtk-layer-shell-devel openssl-devel libdbusmenu-gtk3-devel pulseaudio-libs-devel libinput-devel luajit-devel lua-lgi make cmake gcc libevdev-devel sccache ffmpeg ffmpeg-libs libavutil-devel libavformat-devel
+set --global packages cargo gtk3-devel gtk-layer-shell-devel openssl-devel libdbusmenu-gtk3-devel pulseaudio-libs-devel libinput-devel luajit-devel lua-lgi make cmake gcc libevdev-devel sccache ffmpeg ffmpeg-libs libavutil-devel libavformat-devel libavfilter-devel
 set --global RUSTC_WRAPPER $(which sccache)
 
 sudo dnf install $packages -y
