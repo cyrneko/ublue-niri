150f5f36595c21651d88cca12e45a608d80e5686
cyrneko <cyrus42@disroot.org>
2025-05-07 15:24:18 +0200

    let's hope this doesn't explode!

---
+++ scripts/build-ironbar.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+set --global packages cargo gtk3-devel gtk-layer-shell-devel openssl-devel libdbusmenu-gtk3-devel pulseaudio-libs-devel libinput-devel luajit-devel lua-lgi make cmake gcc libevdev-devel sccache
+set --global repos \'terra,https://repos.fyralabs.com/terra$releasever\' terra-release
+
+dnf install --nogpgcheck --repofrompath $repos 
+
+sudo dnf install $packages -y
+
+git clone --depth 1 https://github.com/JakeStanger/ironbar.git
+cd ironbar
+set --global RUSTC_WRAPPER $(which sccache)
+cargo build --release
+cd ..
+rm -rf ironbar
+
+sudo install target/release/ironbar /usr/bin/ironbar
+
+sudo dnf remove $packages -y
+sudo dnf autoremove
